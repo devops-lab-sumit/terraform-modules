9d0aaa24b375ef178db3450803e042da55789d1a
sumit9315 <sumit.202ps026@nitk.edu.in>
2026-04-19 00:16:29 +0530

    added tag variable

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,11 +5,12 @@ resource "aws_vpc" "main_vpc" {
     cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support = true
-
-    tags = {
-    Name = "terraform_vpc"
-   
-    }
+    tags = merge(
+     var.tags, 
+     {
+     Name = var.vpc_name   
+     }
+)
 }
 
 //public subnet
